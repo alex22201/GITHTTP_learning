85d9c1036c6715753e7a0d193eaa2b0d5b4f46c2
admin <1234>
2021-12-05 01:53:19 +0200

    post zaprosi params,json

--- first/__init__.py
+++ first/__init__.py
@@ -3,9 +3,10 @@ import requests
 headers = {
     "User-Agent": "Glolooooololo"
 }
-response = requests.get("http://httpbin.org/get", headers=headers)
-
+response = requests.post("http://httpbin.org/post", headers=headers,
+                        params={'a': 'b', 'c': 10},
+                        json={'username': 'supersecret'})
 if response.ok:
     print("ok!")
 print(response.text)
-print(response.json()['headers']['User-Agent'])
+# print(response.json()['headers']['User-Agent'])
