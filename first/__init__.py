dce26b9ae3aeae6b4d826970ad0da8e2f7c2641d
admin <1234>
2021-12-05 02:11:12 +0200

    get brouser info write htm to file 1.html

--- first/__init__.py
+++ first/__init__.py
@@ -1,12 +1,17 @@
 import requests
 
-headers = {
-    "User-Agent": "Glolooooololo"
-}
-response = requests.post("http://httpbin.org/post", headers=headers,
-                        params={'a': 'b', 'c': 10},
-                        json={'username': 'supersecret'})
-if response.ok:
-    print("ok!")
-print(response.text)
-# print(response.json()['headers']['User-Agent'])
+#headers = {
+#    "User-Agent": "Glolooooololo"
+#}
+#responce = requests.post("http://httpbin.org/post", headers=headers,
+#                       params={'a': 'b', 'c': 10},
+#                       json={'username': 'supersecret'})
+
+link = "http://omwap.mywibes.com/brouser_info/"
+responce = requests.get(link).text
+
+print(responce)
+with open("./1.html", "w", encoding="utf-8") as file:
+    file.write(responce)
+#print(responce.text)
+# print(responce.json()['headers']['User-Agent'])
